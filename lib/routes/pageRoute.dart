3b2338579634e4c6b1059f9f5ad2ea0112fac8f5
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-05-22 15:57:22 +0530

    Create pageRoute.dart

---
+++ lib/routes/pageRoute.dart
@@ -0,0 +1,18 @@
+// ignore: file_names
+import 'package:skinapp/screens/AboutDiseases/aboutdiseases.dart';
+import 'package:skinapp/screens/Aboutus/aboutcreaters.dart';
+import 'package:skinapp/screens/Auth/Login.dart';
+import 'package:skinapp/screens/Auth/SignUpScreen.dart';
+import '../screens/MainScreen/mainScreen.dart';
+import '../screens/DoandDont/Doees.dart';
+import '../screens/Introduction/intro.dart';
+
+class PageRoutes {
+  static const String signIn = SignInPage.routName;
+  static const String signup = SignUpScreen.routName;
+  static const String home = MainScreen.routName;
+  static const String dodont = Dos.routeName;
+  static const String intro = Intro.routeName;
+  static const String aboutdis = AboutDisease.routeName;
+  static const String aboutus = AboutUs.routeName;
+}
